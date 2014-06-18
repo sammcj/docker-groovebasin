1e9f3c8c17a78e6ccc27f29e9e5fc866d7183c09
John Regan <john@jrjrtech.com>
2014-06-18 15:54:54 -0500

    Hmmm

--- build.sh
+++ build.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update 
-apt-get install -y software-properties-common
-apt-add-repository ppa:andrewrk/libgroove
-apt-add-repository ppa:chris-lea/node.js
-apt-get update
-
-apt-get -y install build-essential git python
-apt-get -y install libgroove-dev libgrooveplayer-dev \
-    libgrooveloudness-dev libgroovefingerprinter-dev \
-    libgroove4 libgrooveplayer4 libgrooveloudness4 libgroovefingerprinter4
-apt-get -y install nodejs
-
-useradd -m groovebasin -G audio 
-mkdir /music && touch /music/.dummy 
-chown groovebasin:groovebasin /music 
-mkdir /groove && touch /groove/.dummy 
-chown groovebasin:groovebasin /groove
-
-ln -s /music /home/groovebasin/music
-ln -s /groove /home/groovebasin/groovebasin.db
-chown groovebasin:groovebasin /home/groovebasin/*
 
 su -c 'git clone https://github.com/andrewrk/groovebasin.git /home/groovebasin/groovebasin-build' \
     -s /bin/bash groovebasin
